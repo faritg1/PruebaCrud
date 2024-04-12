e6e75683135be43b9a64a22f3cd773aa1a8bd8de
faritg1 <juanagelves@gmail.com>
2024-04-11 20:05:44 -0500

    feat: :hammer: Conexion db con patron de diseño singleton

---
+++ database/MySQLi/Connection.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Database\MySQLi;
+
+class Connection{
+    private static $instance;
+    private $connection;
+
+    private function __construct(){
+        $this->make_connection();
+    }
+
+    private static function getInstance(){
+        if(!self::$instance instanceof self){
+            self::$instance = new self();
+        }
+        return self::$instance;
+    }
+
+    public function getDbConnection(){
+        return $this->connection;
+    }
+
+    private function make_connection(){
+        $server = "localhost";
+        $database = "pruebauno";
+        $username = "root";
+        $password = "";
+
+        $conn = new \mysqli($server, $username, $password, $database);
+        
+        if ($conn->connect_errno) {
+            die("Connection failed: " . mysqli_connect_error());
+        }
+
+        $setname = $conn->prepare("SET NAMES 'utf8'");
+        $setname->execute();
+
+        $this->connection = $conn;
+    }
+}
+
+
